e82013e27edbc9f39de4dfd73586f5eed40220a6
Dmitry Andreev <andreev.dmitry.1990@gmail.com>
2019-08-18 10:53:02 +0500

    Improve threads

--- client.c
+++ client.c
@@ -7,67 +7,109 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
+typedef struct
+{
+  int socketDescriptor;
+  struct sockaddr_in serverAddress;
+}
+tUdp;
+
 #define PORT     2115
 #define MSG_SIZE 1000
 
-void *clientThread(void); /* Thread function */
+void udpInit(tUdp *pUdp);
+void sendMsgActivity(tUdp *pUdp);
+void recvMsgThread(void *pUdp); /* Thread function */
 
 int main()
 {
-    pthread_t tid;
-    pthread_attr_t attr;
+  tUdp udp = {};
 
-    /* Get default attributes */
-    pthread_attr_init(&attr);
-    /* Create new thread*/
-    pthread_create(&tid,&attr,(void *)clientThread, NULL);
+  pthread_t tid;
+  pthread_attr_t attr;
 
-    /* Wait for end of thread execution */
-    pthread_join(tid,NULL);
+  udpInit(&udp);
 
-    return 0;
+  /* Get default attributes */
+  pthread_attr_init(&attr);
+  /* Create new thread*/
+  pthread_create(&tid,&attr,(void *)recvMsgThread, &udp);
+
+  sendMsgActivity(&udp);
+  pthread_exit(0);
+  /* Wait for end of thread execution */
+  pthread_join(tid,NULL);
+
+  close(udp.socketDescriptor);
+  return 0;
 }
 
-void *clientThread()
+void udpInit(tUdp *pUdp)
 {
-  int udpSocketDescriptor;
-
-  struct sockaddr_in udpServerAddress = {};
-  char clientMsg[MSG_SIZE];
-
+  int connectionStatus;
   // Creating socket descriptor
-  udpSocketDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
-  if ( udpSocketDescriptor < 0 )
+  pUdp->socketDescriptor = socket(AF_INET, SOCK_DGRAM, 0);
+  if ( pUdp->socketDescriptor < 0 )
   {
       perror("UDP socket creation failed");
       exit(EXIT_FAILURE);
   }
 
   // Filling server information
-  udpServerAddress.sin_family = AF_INET;
-  udpServerAddress.sin_port = htons(PORT);
-  udpServerAddress.sin_addr.s_addr = INADDR_ANY;
+  pUdp->serverAddress.sin_family = AF_INET;
+  pUdp->serverAddress.sin_port = htons(PORT);
+  pUdp->serverAddress.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+  // connect to server
+  connectionStatus = connect(pUdp->socketDescriptor,
+                             (struct sockaddr *)&(pUdp->serverAddress),
+                             sizeof((pUdp->serverAddress)));
+  if(connectionStatus < 0)
+  {
+ 		perror("\n Error : Connect Failed \n");
+ 		exit(EXIT_FAILURE);
+ 	}
+}
+
+void recvMsgThread(void *pUdp)
+{
+  char buffer[MSG_SIZE];
 
   do
   {
-    size_t sendtoStatus;
+    recvfrom(((tUdp*)pUdp)->socketDescriptor,
+             buffer,
+             sizeof(buffer),
+             0,
+             (struct sockaddr*)NULL,
+             NULL);
+    puts(buffer);
+  }
+  while(strcmp(buffer, "/exit\n") != 0);
+}
 
+void sendMsgActivity(tUdp *pUdp)
+{
+  char clientMsg[MSG_SIZE];
+  size_t sendtoStatus;
+
+  do
+  {
     if (fgets(clientMsg, sizeof(clientMsg), stdin) == NULL)
     {
       perror("fgets failed");
       break;
     }
-    else if(strcmp(clientMsg, "/exit\n") == 0)
+
+    if(strcmp(clientMsg, "/exit\n") == 0)
     {
       break;
     }
 
-    sendtoStatus = sendto(udpSocketDescriptor,
-                          (const char *)clientMsg,
+    sendtoStatus = send(pUdp->socketDescriptor,
+                          &clientMsg,
                           strlen(clientMsg),
-                          MSG_CONFIRM,
-                          (const struct sockaddr *) &udpServerAddress,
-                          sizeof(udpServerAddress));
+                          0);
     if (sendtoStatus == -1)
     {
       perror("sendto failed");
@@ -75,7 +117,4 @@ void *clientThread()
     }
   }
   while(1);
-
-  close(udpSocketDescriptor);
-  pthread_exit(0);
 }
